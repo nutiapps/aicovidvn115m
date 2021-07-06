22c778b68c235cc88b711c37cba439a51b6deff1
Truong Lam <nutiapps@gmail.com>
2021-07-06 07:03:18 +0700

    exp run with lgb

--- README.md
+++ README.md
@@ -5,15 +5,16 @@ Our second place solution at aihub.vn.
 ### Steps
 
 ```
-1. copy data and unzip into data/raw
+# 1. run download stage
+dvc run -n download \              
+-d src/create_dataset.py -o data/raw \
+python src/create_dataset.py
 
-2. folder structures
-- src
-- data
-    - raw
-    - features
-    - models
+# 2. run featurization
 
+# 3. run train_predict
+
+# 4. run evaluate to see the local cv5 score
 ```
 
 ### License
--- src/evaluate.py
+++ src/evaluate.py
@@ -1,10 +1,16 @@
 import numpy as np
-from sklearn.metrics import f1_score, confusion_matrix, roc_auc_score, auc, precision_recall_curve
+from sklearn.metrics import f1_score, confusion_matrix, roc_auc_score, auc, precision_recall_curve, roc_curve, average_precision_score
 from sklearn.model_selection import StratifiedKFold
 from tabulate import tabulate
 import pandas as pd
 import json
 import h5py
+import math
+import yaml
+
+import xgboost as xgb
+import lightgbm as lgb
+from sklearn.ensemble import ExtraTreesClassifier
 
 from models import LittleLamb
 from config import Config
@@ -40,7 +46,7 @@ def reporting(ensem_preds, targets):
     specificity1 = cm1[1,1]/(cm1[1,0]+cm1[1,1])
     print(f'Specificity : {specificity1:12.4f}')
 
-def fit_and_predict(model, X,y, X_test, columns, seed=42):
+def fit_and_predict(model, X,y, X_test, columns, seed=42, is_single=False):
     ensem_preds = []
     ensem_tests = []
     print("[*] model", model.__class__)
@@ -55,11 +61,16 @@ def fit_and_predict(model, X,y, X_test, columns, seed=42):
         X_val = X[valid_idx]
         y_val = y[valid_idx]
         targets.append(y_val)
-        print(f"train ratio {np.mean(y_train):.02f} val ratio {np.mean(y_val):.02f}")
+        print(f"fold {fold} train ratio {np.mean(y_train):.02f} val ratio {np.mean(y_val):.02f}")
         model.fit(X_train, y_train)
-        preds.append(model.predict_proba(X_val))
-        test_preds.append(model.predict_proba(X_test))
-        aucs.append(roc_auc_score(y_val, model.predict_proba(X_val)))
+        if is_single:
+            preds.append(model.predict_proba(X_val)[:,1])
+            aucs.append(roc_auc_score(y_val, model.predict_proba(X_val)[:,1]))
+            test_preds.append(model.predict_proba(X_test)[:,1])
+        else:
+            preds.append(model.predict_proba(X_val))
+            aucs.append(roc_auc_score(y_val, model.predict_proba(X_val)))
+            test_preds.append(model.predict_proba(X_test))
     
     targets = np.concatenate(targets)
     preds = np.concatenate(preds)
@@ -79,8 +90,22 @@ def fit_and_predict(model, X,y, X_test, columns, seed=42):
     return ensem_preds, targets, ensem_tests
 
 if __name__ == '__main__':
+    trial = yaml.safe_load(open('params.yaml'))['evaluation']['trials']
+    seed = yaml.safe_load(open('params.yaml'))['evaluation']['seed']
+    model_index = yaml.safe_load(open('params.yaml'))['evaluation']['model_index']
+    collection_index = yaml.safe_load(open('params.yaml'))['evaluation']['collection_index']
+    n_est = yaml.safe_load(open('params.yaml'))['evaluation']['n_est']
+    cbt = yaml.safe_load(open('params.yaml'))['evaluation']['cbt']
+    ss = yaml.safe_load(open('params.yaml'))['evaluation']['ss']
+
     # load feature
-    f = h5py.File(str(Config.FEATURES_PATH / "collection5.h5"), 'r')
+    if collection_index == 2:
+        f = h5py.File(str(Config.FEATURES_PATH / "collection2.h5"), 'r')
+    elif collection_index == 3:
+        f = h5py.File(str(Config.FEATURES_PATH / "collection3.h5"), 'r')
+    else:
+        f = h5py.File(str(Config.FEATURES_PATH / "collection5.h5"), 'r')
+
     X = np.array(f["X"])
     X_test = np.array(f["X_test"])
     y = np.array(f["y"])
@@ -89,31 +114,87 @@ if __name__ == '__main__':
     pos_scale = float(np.array(f.get('pos_scale')))
 
     # load model
-    trial = 0
-    model = LittleLamb(seed=(2024 + 10*trial), nest_lgb=2., nest_xgb=2., nest_et=2., cbt=0.5, ss=0.5, pos_scale=pos_scale)
-    ensem_preds, targets, ensem_tests = fit_and_predict(model, X, y.astype(int), X_test, columns, 42)
+    if model_index == 1:
+        model = lgb.LGBMClassifier(class_weight='balanced', 
+                                            num_leaves=31, 
+                                            max_depth=-1, 
+                                            min_child_samples=2, 
+                                            learning_rate=0.02,
+                                            n_estimators=n_est, 
+                                            colsample_bytree=cbt, 
+                                            subsample=ss, 
+                                            n_jobs=-1, 
+                                            random_state=0+seed
+                                           )
+    elif model_index == 2:
+        modle = xgb.XGBClassifier(max_depth=12,
+                                           scale_pos_weight=1.,
+                                           learning_rate=0.01, 
+                                           n_estimators=n_est,
+                                           subsample=ss, 
+                                           colsample_bytree=cbt,
+                                           nthread=-1,
+                                           seed=0+seed,
+                                           eval_metric='logloss'
+                                          )
+    elif model_index == 3:
+        model = ExtraTreesClassifier(class_weight='balanced', 
+                                              bootstrap=False, 
+                                              criterion='entropy', 
+                                              max_features=cbt, 
+                                              min_samples_leaf=4, 
+                                              min_samples_split=3, 
+                                              n_estimators= n_est, 
+                                              random_state=0+seed, 
+                                              n_jobs=-1)
+    else:
+        model = LittleLamb(seed=(seed + 10*trial), nest_lgb=2., nest_xgb=2., nest_et=2., cbt=0.5, ss=0.5, pos_scale=pos_scale)
+    
+    if model_index > 0:
+        ensem_preds, targets, ensem_tests, fim = fit_and_predict(model, X, y.astype(int), X_test, columns, seed=seed, is_single=True)
+        fim.to_json(str(Config.FI_FILE_PATH))
+    else:
+        ensem_preds, targets, ensem_tests = fit_and_predict(model, X, y.astype(int), X_test, columns, seed=seed, is_single=False)
+
 
     ensem_tests = np.array(ensem_tests).mean(axis=0)
     ensem_preds = np.array(ensem_preds).mean(axis=0)
     reporting(ensem_preds, targets)
 
     # generate scores
-    precision, recall, thresholds = precision_recall_curve(targets, ensem_preds)
-    auc = auc(recall, precision)
-
-    # save feature importance
-    # fi.to_json(str(Config.FI_FILE_PATH), index=None)
+    precision, recall, prc_thresholds = precision_recall_curve(targets, ensem_preds)
+    fpr, tpr, roc_thresholds = roc_curve(targets, ensem_preds)
+    avg_prec = average_precision_score(targets, ensem_preds)
+    roc_auc = roc_auc_score(targets, ensem_preds)
 
     # save scores
-    with open(str(Config.METRICS_FILE_PATH), 'w') as f:
-        json.dump({'auc': auc}, f)
+    with open(str(Config.METRICS_FILE_PATH), 'w') as fd:
+        json.dump({"avg_prec": avg_prec, "roc_auc": roc_auc}, fd, indent=4)
+
+    nth_point = math.ceil(len(prc_thresholds) / 1000)
+    prc_points = list(zip(precision, recall, prc_thresholds))[::nth_point]
 
     # save plots
-    with open(str(Config.PLOT_FILE_PATH), 'w') as f:
-        proc_dict = {'proc': [{
-            'precision': p,
-            'recall': r,
-            'threshold': t
-            } for p, r, t in zip(precision, recall, thresholds)
-        ]}
-        json.dump(proc_dict, f)
+    with open(str(Config.PRC_FILE_PATH), 'w') as fd:
+        json.dump(
+        {
+            "prc": [
+                {"precision": p, "recall": r, "threshold": t}
+                for p, r, t in prc_points
+            ]
+        },
+        fd,
+        indent=4,
+    )
+
+    with open(str(Config.ROC_FILE_PATH), 'w') as fd:
+        json.dump(
+        {
+            "roc": [
+                {"fpr": fp, "tpr": tp, "threshold": t}
+                for fp, tp, t in zip(fpr, tpr, roc_thresholds)
+            ]
+        },
+        fd,
+        indent=4,
+    )
